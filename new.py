8e477ca18d1742f1050ff17aa07ecf89c79a24c1
Daqwayne <anhchangchem159@gmail.com>
2022-12-03 11:51:27 +0700

    add new stuff
    . add new logo
    . change my_form options to multiselect
    . add search_form
    . change do_not_click button

--- new.py
+++ new.py
@@ -7,9 +7,17 @@ import pandas as pd
 import webbrowser
 from PIL import Image
 
+
+
 col1, col2, col3 = st.columns((2,4,1))
 with col1:
-    st.image(Image.open('logo.png'))
+    st.image(Image.open('dark_logo.png'))
+    # if st.theme():
+    #     st.image(Image.open('logo.png'))
+    # else:
+    #     st.image(Image.open('dark_logo.png'))
+
+# st.plotly_chart(fig.update_layout(theme))
 with col2:
     st.header(' ')
     st.header('Knowledge Represent')
@@ -19,34 +27,34 @@ with col2:
 st.markdown('# 30 days of NNN is over')
 
 with st.form("my_form"):
-    # st.write("Inside the form")
-
-#    # Every form must have a submit button.
-#      submitted = st.form_submit_button("???")
-#      if submitted:
-#           st.write("slider", slider_val, "checkbox", checkbox_val)
-    option = st.selectbox(
+    option = st.multiselect(
         'Quận',
         ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 
         'Tân Bình', 'Bình Tân', 'Tân Phú', 'Bình Thạnh', 'Gò Vấp', 'Phú Nhuận',
         'Hóc Môn', 'Bình Chánh', 'Nhà Bè', 'Củ Chi'))
 
-    st.write('You selected:', option)
-    if option == '1':
-        st.write("ho ho ho")
-     
-    # image = Image.open('dalat.jpg')
-    # st.image(image, caption='Sunrise by the mountains')
-    # submitted = st.form_submit_button("Search")
-    submitted = st.form_submit_button("Search")
-    if submitted:
-        st.write("your mom is a hoe")
-# st.write("Outside the form")
-# # submitted = st.button('submit')
+    submitted = st.form_submit_button("Next")
+
+if not option and submitted==True:
+    st.warning("Hãy chọn quận!!",icon="⚠️")
+
+if option:
+    with st.form("serching_form"):
+        st.write('Quận đã chọn: ', ', '.join(option))   
+        with st.expander("filter"):
+        # st.text("Filter")
+            money = st.slider('Chọn giá tiền (triệu/m²)',0, 200, (0, 50))
+        search = st.form_submit_button("Search")
+        if search:
+            st.write('Quận đã chọn: ', ', '.join(option))
+            # st.select_slider("Displayed values:", ["Normalized", "Absolute"])
+
+
 
 url = 'https://youtu.be/dQw4w9WgXcQ'
 
-if st.button('do not click this'):
+if st.button('_do not click_ **this**',type='primary'):
     webbrowser.open_new_tab(url)
+    st.caption('i told ya')
 
 
